3c104450cf3443a810edd7119765c00a9e07b21a
santhiya-622 <vsanthiya022@gmail.com>
2025-04-01 10:03:53 +0530

    Moved src and public folders to main repository

---
+++ src/src/components/Navbar.js
@@ -0,0 +1,17 @@
+function Navbar()
+{
+  return(
+    <nav>
+      <h1>perfumy </h1>
+      <ul>
+        <li>Home </li>
+        <li>Products</li>
+        <li>About</li>
+        <li>contacts</li>
+      </ul>
+    </nav>
+
+  )
+}
+
+export default Navbar
---
+++ src/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import "./style.css"
+import App from "./App.js";
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+
+
+root.render(
+  <App></App>)
